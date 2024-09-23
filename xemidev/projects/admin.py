948642ed979fefef0d42a46e5fb6759cbfc95288
Victor GC <vgarcanuk@gmail.com>
2024-09-23 23:16:43 +0100

    UPDATE: admin adjusted for DB display.

--- xemidev/projects/admin.py
+++ xemidev/projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Project, SelectedTech, Category
+
+admin.site.register(Project)
+admin.site.register(SelectedTech)
+admin.site.register(Category)
